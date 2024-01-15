b431b256850db34d453e7e2dbd801635baf541d6
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2024-01-15 14:09:10 +0700

    update home dashboard UI

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,7 @@ import { CiUser } from "react-icons/ci";
 
 const Header = () => {
   return (
-    <header className="bg-[#567582] flex justify-center items-center text-white p-5 shadow-lg">
+    <header className="bg-[#567582] flex justify-center items-center text-white p-5 shadow-lg sticky top-0 z-50">
       <div className="container flex justify-between items-center">
         <section>
           <a href="/">DEUR</a>
